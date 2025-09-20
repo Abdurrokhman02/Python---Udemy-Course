b550ec11bc2b9a12736865acd2ba4f62bcef9520
Abdurrokhman <148690309+Abdurrokhman02@users.noreply.github.com>
2025-09-20 16:12:38 +0700

    Update views.py

--- Django/weatherapp/weather/views.py
+++ Django/weatherapp/weather/views.py
@@ -6,6 +6,6 @@ import requests
 def weather(request):
     if request.method == 'POST':
         city = request.POST['city']
-        source = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=70306bce3fe6b82cdb3de3b2393e5401"
+        source = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=api"
         list_of_data = requests.get(source.format(city)).json()
         
